c2ebb73632cd327e9d2087235414711f7c629c21
Attila Szeremi <attila@szeremi.org>
2016-09-10 23:18:59 +0200

    Add button for adding rows and columns

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
   <div id="main">
-    <table class="skillgrid">
-      <tr v-for="i in 4">
-        <td v-for="j in 4" class="skillgrid__td">
-          &nbsp;
-        </td>
-      </tr>
-    </table>
+    <div>
+      Rows:
+      <button type="button" @click="addRow()">+</button>
+    </div>
+
+    <div>
+      Columns:
+      <button type="button" @click="addColumn()">+</button>
+
+    </div>
+    <div>
+      <table class="skillgrid">
+        <tr v-for="i in table" track-by="$index">
+          <td v-for="j in i" track-by="$index" class="skillgrid__td">
+            &nbsp;
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+const emptyValue = '&nbsp'
+const data = () => ({
+  table: Array(4).fill(
+    Array(4).fill(emptyValue)
+  )
+})
+export default {
+  data,
+  methods: {
+    addRow () {
+      const columns = this.table.length
+        ? this.table[0].length
+        : 0
+      this.table.push(Array(columns).fill(emptyValue))
+    },
+    addColumn () {
+      this.table[0].push(emptyValue)
+    }
+  }
+}
 </script>
 
 <style>
@@ -52,6 +84,6 @@ body {
     border: 1px solid grey;
     padding: 5px;
     width: 50px;
-    height: 100px;
+    height: 50px;
   }
 </style>
